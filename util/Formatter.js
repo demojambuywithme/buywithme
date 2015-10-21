d15efea4937b452da25ba0baa28eef42212748cb
Jiang, Xin <jiangxin0503@163.com>
2015-10-21 15:52:13 +0800

    adjust the discount information data formater

--- util/Formatter.js
+++ util/Formatter.js
@@ -2,6 +2,21 @@ jQuery.sap.declare("bwm.util.Formatter");
 
 bwm.util.Formatter = {
 	getImageSrc : function(imageData) {
-		return "data:image/png;base64,"+imageData;
+		return "data:image/png;base64," + imageData;
+	},
+
+	discountInforFormatter : function(total_quantity,discountType,discount,total_money,return_money) {
+		
+		var returnText = '';
+		
+		if (discountType === '01') {
+			returnText = 'Buy ' + total_money + ' Return ' + return_money;
+		}else if(discountType === '02') {
+			var off = (1 - discount) * 100;
+			returnText = 'Buy ' + total_quantity + ' PC ' + off.toString() + '%' ;
+		}
+		
+		return returnText;
 	}
+	
 };
